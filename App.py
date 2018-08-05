f5b1f64ea42952d99268adfc7509cc5a0ae1c9f0
Raghuveer Singh <raghu1213@hotmail.com>
2018-08-05 21:32:36 +0800

    Initial commit

---
+++ App.py
@@ -0,0 +1,25 @@
+import logging
+from codeitsuisse import  app
+logger = logging.getLogger(__name__)
+
+@app.route('/', methods=['GET'])
+def default_route():
+    return "Team XYZ page";
+
+if __name__ == "__main__":
+    logFormatter = logging.Formatter("%(asctime)s [%(filename)s] [%(funcName)s] [%(lineno)d] [%(levelname)-5.5s]  %(message)s")
+    rootLogger = logging.getLogger()
+
+    rootLogger.setLevel(logging.INFO)
+
+    fileHandler = logging.FileHandler("team.log")
+    fileHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(fileHandler)
+
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(consoleHandler)
+
+    logger.info("Starting application ...")
+    app.run()
+
---
+++ codeitsuisse/routes/challenge.py
@@ -0,0 +1,19 @@
+import logging
+
+from flask import request, jsonify;
+
+from codeitsuisse import app;
+
+logger = logging.getLogger(__name__)
+
+@app.route('/challenge', methods=['POST'])
+def evaluate():
+    data = request.get_json();
+    logging.info("data sent for evaluation {}".format(data))
+    inputValue = data.get("input");
+    result = inputValue * inputValue
+    logging.info("My result :{}".format(result))
+    return jsonify(result);
+
+
+
