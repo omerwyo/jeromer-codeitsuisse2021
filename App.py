d3fe769b795584fb9e904b47d6f36122e5d8b22b
Raghuveer Singh <raghu1213@hotmail.com>
2018-09-22 11:08:12 +0800

    logging

--- App.py
+++ App.py
@@ -6,20 +6,21 @@ logger = logging.getLogger(__name__)
 def default_route():
     return "Team XYZ page";
 
+logFormatter = logging.Formatter("%(asctime)s [%(filename)s] [%(funcName)s] [%(lineno)d] [%(levelname)-5.5s]  %(message)s")
+rootLogger = logging.getLogger()
+
+rootLogger.setLevel(logging.INFO)
+
+fileHandler = logging.FileHandler("team.log")
+fileHandler.setFormatter(logFormatter)
+rootLogger.addHandler(fileHandler)
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logFormatter)
+rootLogger.addHandler(consoleHandler)
+
+logger.info("Starting application ...")
+
 if __name__ == "__main__":
-    logFormatter = logging.Formatter("%(asctime)s [%(filename)s] [%(funcName)s] [%(lineno)d] [%(levelname)-5.5s]  %(message)s")
-    rootLogger = logging.getLogger()
-
-    rootLogger.setLevel(logging.INFO)
-
-    fileHandler = logging.FileHandler("team.log")
-    fileHandler.setFormatter(logFormatter)
-    rootLogger.addHandler(fileHandler)
-
-    consoleHandler = logging.StreamHandler()
-    consoleHandler.setFormatter(logFormatter)
-    rootLogger.addHandler(consoleHandler)
-
-    logger.info("Starting application ...")
-    app.run()
+    app.run();
 
