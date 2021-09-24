4bbf75aa7a9c9d4116451343b98182d5087c55f5
Dave Ip <davidjwip@gmail.com>
2021-09-24 14:54:24 +0000

    removed semi-colon

--- App.py
+++ App.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 
 @app.route('/', methods=['GET'])
 def default_route():
-    return "Python Template";
+    return "Python Template"
 
 
 logger = logging.getLogger()
