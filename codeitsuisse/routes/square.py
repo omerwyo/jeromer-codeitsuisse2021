69f1995c03eff5cb9f4d2a897730f4b542362597
Dave Ip <davidjwip@gmail.com>
2021-09-24 14:57:37 +0000

    Clearing more semi-colons

--- codeitsuisse/routes/square.py
+++ codeitsuisse/routes/square.py
@@ -1,20 +1,20 @@
 import logging
 import json
 
-from flask import request, jsonify;
+from flask import request, jsonify
 
-from codeitsuisse import app;
+from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
 
 @app.route('/square', methods=['POST'])
 def evaluate():
-    data = request.get_json();
+    data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input");
+    inputValue = data.get("input")
     result = inputValue * inputValue
     logging.info("My result :{}".format(result))
-    return json.dumps(result);
+    return json.dumps(result)
 
 
 
