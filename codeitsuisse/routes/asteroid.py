a0ec998db449ad735daa24a3ea784249b9108a2d
omerwyo <omerbaggia123@gmail.com>
2021-09-25 20:20:57 +0800

    asteroid

--- codeitsuisse/routes/asteroid.py
+++ codeitsuisse/routes/asteroid.py
@@ -24,6 +24,7 @@ def asteroid():
     logging.info("My result :{}".format(outputList))
     return json.dumps(outputList)
 
+# hardcoded one
 def originFinder(inputStr):
     chunkList = []
     runningChar = inputStr[0]
@@ -46,26 +47,53 @@ def originFinder(inputStr):
     print('Origin' + str(origin))
     return origin
     
-def calcScore(index, inputStr):
-    pass 
 
-def asteroidCalculator(inputStr):
-    ddict = {}
-    for i in range(len(inputStr)):
-        if inputStr[i] in ddict:
-            ddict[inputStr[i]] = ddict[inputStr[i]] + 1
-        else:
-            ddict[inputStr[i]] = 1
-    result = 0
-    for key in ddict.keys():
-        value = ddict[key]
-        if value >= 10:
-            multiplier = 2
-        elif value >= 7:
-            multiplier = 1.5
-        else:
-            multiplier = 1
-        result += multiplier * value
-    print(int(result))
-    return int(result)
+def asteroid2(inputStr):
+    chunkList = []
+    runningChar = inputStr[0]
+    runningStr = ''
+    for char in inputStr:
+        if char != runningChar:
+            runningChar = char
+            chunkList.append(runningStr)
+            runningStr = ''
+        runningStr += char
+    chunkList.append(runningStr)
+    print(chunkList)
+    myStr = chunkList[(len(chunkList) // 2)]
+
+    asteroidCalc(chunkList)
+
+def chunk_calc(chunk):
+    score = len(chunk)
+    if score >= 10:
+        score *= 2
+    elif score >= 7:
+        score *= 1.5
+    return score
+
+def chunk_merge(chunkList):
+    listLen = len(chunkList) - 1
+    for i in range(listLen):
+
+        if(i==1):
+            return chunkList
+        if chunkList[i][0] == chunkList[i+1][0]:
+            chunkList[i] = chunkList[i] + chunkList[i+1]
+            chunkList.pop(i+1)
+            listLen -= 1
+    return chunkList
+
+def asteroidCalc(chunkList,total_score=0):
+    possible_value = []
+    if(len(chunkList) == 1):
+        return chunk_calc(chunkList[0])
+    for i in range(len(chunkList)):
+        score = chunk_calc(chunkList[i])
+        chunkListCopy = [chunk for chunk in chunkList]
+        chunkListCopy.pop(i)
+        chunkListCopy = chunk_merge(chunkListCopy)
+        possible_value.append(asteroidCalc(chunkListCopy, total_score + score))
+    return max(possible_value)
+
         
