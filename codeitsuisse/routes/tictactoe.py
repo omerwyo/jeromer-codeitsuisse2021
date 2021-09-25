093d7b41b2a2fb132d2d67ef3b659cd00e6a44bc
Jerome Ng <jerome2116@hotmail.com>
2021-09-25 15:26:42 +0800

    Running Test v1

--- codeitsuisse/routes/tictactoe.py
+++ codeitsuisse/routes/tictactoe.py
@@ -3,6 +3,7 @@ import json
 import requests
 import sseclient
 from urllib.parse import urlparse
+from random import randint
 
 from flask import request, jsonify
 
@@ -12,17 +13,75 @@ logger = logging.getLogger(__name__)
 
 moves = ["NW", "N", "NE", "W", "C", "E", "SW", "S", "SE"]
 
-board = []
-for i in range(9):
-    board.append('')
-
 
 def with_requests(url, headers):
     return requests.get(url, stream=True, headers=headers)
 
 
+def send_post(url, data):
+    requests.post(url=url, data=data, headers={
+                  "Content-Type": "application/json"})
+
+
+def winning_move(player, board):
+    if(board[0] == player and board[1] == player):
+        return 2
+    elif(board[1] == player and board[2] == player):
+        return 0
+    elif(board[0] == player and board[2] == player):
+        return 1
+
+    elif(board[3] == player and board[4] == player):
+        return 5
+    elif(board[4] == player and board[5] == player):
+        return 3
+    elif(board[3] == player and board[5] == player):
+        return 4
+
+    elif(board[6] == player and board[7] == player):
+        return 8
+    elif(board[7] == player and board[8] == player):
+        return 6
+    elif(board[6] == player and board[8] == player):
+        return 7
+
+    elif(board[0] == player and board[4] == player):
+        return 8
+    elif(board[4] == player and board[8] == player):
+        return 0
+    elif(board[0] == player and board[8] == player):
+        return 4
+
+    elif(board[2] == player and board[4] == player):
+        return 6
+    elif(board[4] == player and board[6] == player):
+        return 2
+    elif(board[2] == player and board[6] == player):
+        return 4
+    else:
+        return None
+
+
+def next_move(player, opponent, board):
+    move = winning_move(player, board)
+    if(move != None):
+        return move
+    move = winning_move(opponent, board)
+    if(move != None):
+        return move
+
+    move = randint(0, 8)
+    while(board[move] != ""):
+        move = randint(0, 8)
+    return move
+
+
 @app.route('/tic-tac-toe', methods=['POST'])
 def tic_tac_toe():
+    board = []
+    for i in range(9):
+        board.append('')
+
     data = request.get_json()
     logging.info("BattleId {}".format(data))
     battle_id = data.get("battleId")
@@ -30,18 +89,43 @@ def tic_tac_toe():
     arena_endpoint = "https://cis2021-arena.herokuapp.com/" + "tic-tac-toe/"
 
     logging.info("Arena Endpoint :{}".format(arena_endpoint))
-    play(arena_endpoint, battle_id=battle_id)
+    play(arena_endpoint, battle_id, board)
 
 
-def play(remote_addr, battle_id):
+def play(remote_addr, battle_id, board):
     headers = {'Accept': 'text/event-stream'}
     battle_addr_start = remote_addr + "start/" + battle_id
     battle_addr_play = remote_addr + "play/" + battle_id
     logging.info("Arena Endpoint :{}".format(battle_addr_start))
 
-    response = with_requests(battle_addr_start, headers)
+    res = with_requests(battle_addr_start, headers)
 
-    client = sseclient.SSEClient(response)
+    client = sseclient.SSEClient(res)
+
+    player = ""
+    opponent = ""
     for event in client.events():
-        logging.info("Events running: {}".format(event))
-        logging.info(json.loads(event.data))
+        logging.info("Events running: {}".format(json.loads(event.data)))
+
+        data = json.loads(event.data)
+        if('youAre' in data):
+            player = data['youAre']
+            if player == "X":
+                opponent = "O"
+                continue
+            else:
+                opponent = "X"
+
+        if("action" in data):
+            if(data["action"] != "putSymbol" or data["action" != "(╯°□°)╯︵ ┻━┻"]):
+                send_post(battle_addr_play, {"action": "(╯°□°)╯︵ ┻━┻"})
+                continue
+            board[moves.index(data["position"])] = data["player"]
+
+        if("winner" in data):
+            logger.info(data)
+
+        move = next_move(player, opponent, board)
+        board[move] = player
+        send_post(battle_addr_play, {
+            "action": "putSymbol", "position": moves[move]})
